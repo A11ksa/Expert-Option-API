7905c977128e0d29fc72a4a3a010fcd00df2f0ed
A11ksa <huugme@gmail.com>
2025-05-05 01:43:37 +0300

    __init__.py

--- ExpertOptionsToolsV2/expertoption/__init__.py
+++ ExpertOptionsToolsV2/expertoption/__init__.py
@@ -1,1 +1,12 @@
+"""
+Module for Expert Option related functionality.
 
+Contains asynchronous and synchronous clients,
+as well as specific classes for Expert Option trading.
+"""
+
+__all__ = ['asyncronous', 'syncronous', 'ExpertOptionAsync', 'ExpertOption']
+
+from . import asyncronous, syncronous
+from .asyncronous import ExpertOptionAsync
+from .syncronous import ExpertOption
